150be19f5b3b35f30d210b2df275eca4924fb600
Petter <petterlewithoneeandtwot@gmail.com>
2025-07-24 14:28:57 -0500

    first

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views  # rename to avoid confusion
+from . import views  # your app's views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path("", views.post_list, name="post_list"),
+    path('post/new/', views.post_new, name='post_new'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),   # auth_views.LoginView here
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,89 @@
+from datetime import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render
+from .forms import PostForm
+from .forms import CommentForm
+from .models import Post, Comment
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method=='POST':
+        post.delete()
+    return redirect('post_list')
+
+@login_required
+def post_new(request):
+    [...]
+
+@login_required
+def post_edit(request):
+    [...]
+
+@login_required
+def post_draft_list(request):
+    [...]
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method=='POST':
+        post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+            
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
